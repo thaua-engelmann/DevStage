87b17aae0edf13777f8d9c3aefa5f7382fa402c4
thaua-engelmann <engelmann.webdev@gmail.com>
2025-02-22 02:25:00 -0300

    config(DevStage.Api): configure DbContext

---
+++ DevStage.Api/Infrastructure/DevStageDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DevStage.Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevStage.Api.Infrastructure
+{
+    public class DevStageDbContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\engel\\OneDrive\\Documents\\DevStageDB.db");
+        }
+
+    }
+}
