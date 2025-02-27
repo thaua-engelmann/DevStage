ea6070bf3140415941219373c199ab397c9ee047
thaua-engelmann <engelmann.webdev@gmail.com>
2025-02-27 20:37:56 -0300

    feat(LoginController): create logic for user login

--- DevStage.Api/Infrastructure/Security/Cryptography/BCryptAlgorithms.cs
+++ DevStage.Api/Infrastructure/Security/Cryptography/BCryptAlgorithms.cs
@@ -3,5 +3,7 @@
     public class BCryptAlgorithms
     {
         public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+
+        public bool VerifyPassword(string password, string hashPassword) => BCrypt.Net.BCrypt.Verify(password, hashPassword);
     }
 }
